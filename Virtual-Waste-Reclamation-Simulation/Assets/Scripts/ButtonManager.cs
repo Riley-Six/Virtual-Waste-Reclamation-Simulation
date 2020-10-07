e426888b4c80c48c2d0d87a1a660e93828f93d6f
fjhernan <60201738+fjhernan@users.noreply.github.com>
2020-10-07 15:28:03 -0700

    Button rework
    Reworked the buttons to rely on a manager.

---
+++ Virtual-Waste-Reclamation-Simulation/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    private string binInfo;
+    private string sortInfo;
+    private string splitInfo;
+    private string convInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        convInfo = "Hey";
+        splitInfo = "whats";
+        sortInfo = "up";
+        binInfo = "Sponsored by Raid Shadow Legends";
+    }
+
+    public void binClick()
+    {
+        GameObject.Find("Info").GetComponent<Info>().changeInfo(binInfo);
+    }
+    public void sortClick()
+    {
+        GameObject.Find("Info").GetComponent<Info>().changeInfo(sortInfo);
+    }
+    public void splitClick()
+    {
+        GameObject.Find("Info").GetComponent<Info>().changeInfo(splitInfo);
+    }
+    public void convClick()
+    {
+        GameObject.Find("Info").GetComponent<Info>().changeInfo(convInfo);
+    }
+}
