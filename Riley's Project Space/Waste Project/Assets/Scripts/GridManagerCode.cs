3d85798f9a384bb60bb3bb555e7bb4d05b96ba37
Riley-Six <60455700+Riley-Six@users.noreply.github.com>
2020-10-13 17:14:00 -0700

    Colors Change!!!
    I finished the color palette swapping code.

--- Riley's Project Space/Waste Project/Assets/Scripts/GridManagerCode.cs
+++ Riley's Project Space/Waste Project/Assets/Scripts/GridManagerCode.cs
@@ -30,7 +30,7 @@ public class GridManagerCode : MonoBehaviour
             for (int runnerY = -collems; runnerY < collems; runnerY++)
             {
                 GameObject tile = (GameObject)Instantiate(blankSpace, transform);
-                tile.name = "TempTile " + "(" + counter + ")";
+                tile.name = "tempTile" + counter;
                 counter++;
                 float xPos = runnerY * tileSize + offset;
                 float yPos = runnerX * -tileSize + offset -1;
@@ -51,18 +51,17 @@ public class GridManagerCode : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 
 
-            if (hit && hit.collider.gameObject.name.Contains("TempTile"))
+            if (hit && hit.collider.gameObject.name.Contains("tempTile"))
             {
                 print(hit.collider.name);
 
                 Vector3 center = hit.collider.gameObject.transform.position;
-                Destroy(GameObject.Find(hit.collider.gameObject.name));
+                Destroy(hit.collider.gameObject);
                 GameObject newTile = (GameObject)Instantiate(tileTexture, transform);
-                newTile.name = "newTile " + "(" + newCounter + ")";
+                //newTile.name = "newTile" + newCounter;
+                newTile.name = "newTile";
                 newCounter++;
                 newTile.transform.position = center;
-
-
 
                 //GameObject newTile = (GameObject)Instantiate(blankSpace, GameObject.Find(hit.collider.gameObject.name.position));
                 //Destroy(GameObject.Find(hit.collider.gameObject.name));
@@ -75,10 +74,34 @@ public class GridManagerCode : MonoBehaviour
                 //Vector3 tp = GameObject.Find(hit.collider.gameObject.name).positon;
                 //Destroy(GameObject.Find(hit.collider.gameObject.name));
                 //GameObject replacing = (GameObject)Instantiate(tileTexture, tp);
-
-
-
             }
+
+            if (hit && hit.collider.gameObject.name.Contains("newTile"))
+            {
+                print(hit.collider.name);
+
+                Vector3 center = hit.collider.gameObject.transform.position;
+                Destroy(hit.collider.gameObject);
+                GameObject newTile = (GameObject)Instantiate(blankSpace, transform);
+                //newTile.name = "tempTile" + newCounter;
+                newTile.name = "tempTile";
+                newCounter++;
+                newTile.transform.position = center;
+
+                //GameObject newTile = (GameObject)Instantiate(blankSpace, GameObject.Find(hit.collider.gameObject.name.position));
+                //Destroy(GameObject.Find(hit.collider.gameObject.name));
+
+                //Destroy(GameObject.Find(hit.collider.gameObject.name));
+                //GameObject.Find(hit.collider.gameObject.name) = (GameObject)Instantiate(tileTexture, transform);
+                //GameObject tempHolder = GameObject.Find(hit.collider.gameObject.name);
+                //tempHolder = (GameObject)Instantiate(tileTexture, transform);
+                //transform.position = new Vector2();
+                //Vector3 tp = GameObject.Find(hit.collider.gameObject.name).positon;
+                //Destroy(GameObject.Find(hit.collider.gameObject.name));
+                //GameObject replacing = (GameObject)Instantiate(tileTexture, tp);
+            }
+
+
 
             /*
             if (Physics.Raycast(ray, out hit))
