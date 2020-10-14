4190cd6d614bb59f2222a7bfe5439ce21b480060
fjhernan <60201738+fjhernan@users.noreply.github.com>
2020-10-14 16:07:09 -0700

    Revert "Added some more ui features"
    This reverts commit 7725fd58f00a978a8f2df20f9a1a5686b5a08f4e.

--- Virtual-Waste-Reclamation-Simulation/Assets/Scripts/ButtonManager.cs
+++ Virtual-Waste-Reclamation-Simulation/Assets/Scripts/ButtonManager.cs
@@ -8,12 +8,6 @@ public class ButtonManager : MonoBehaviour
     private string sortInfo;
     private string splitInfo;
     private string convInfo;
-    public Sprite convSprite;
-    public Sprite sortSprite;
-    public Sprite binSprite;
-    //public Sprite splitSprite;
-    public GameObject info;
-    private Info infoComponent;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,26 +15,22 @@ public class ButtonManager : MonoBehaviour
         splitInfo = "whats";
         sortInfo = "up";
         binInfo = "Sponsored by Raid Shadow Legends";
-        infoComponent = info.GetComponent<Info>();
     }
 
-    public void convClick(){
-        infoComponent.changeInfo(convInfo, Color.cyan);
-        infoComponent.changeSprite(convSprite);
-        GameObject.Find("SpawnManager").GetComponent<SpawnManager>().SetSpawn("conveyer");
+    public void binClick()
+    {
+        GameObject.Find("Info").GetComponent<Info>().changeInfo(binInfo);
     }
-    public void splitClick(){
-        infoComponent.changeInfo(splitInfo, Color.green);
-        //infoComponent.changeSprite(splitSprite);
+    public void sortClick()
+    {
+        GameObject.Find("Info").GetComponent<Info>().changeInfo(sortInfo);
     }
-
-    public void sortClick(){
-        infoComponent.changeInfo(sortInfo, Color.blue);
-        infoComponent.changeSprite(sortSprite);
+    public void splitClick()
+    {
+        GameObject.Find("Info").GetComponent<Info>().changeInfo(splitInfo);
     }
-
-    public void binClick(){
-        infoComponent.changeInfo(binInfo, Color.red);
-        infoComponent.changeSprite(binSprite);
+    public void convClick()
+    {
+        GameObject.Find("Info").GetComponent<Info>().changeInfo(convInfo);
     }
 }
