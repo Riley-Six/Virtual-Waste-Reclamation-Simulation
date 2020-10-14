7725fd58f00a978a8f2df20f9a1a5686b5a08f4e
fjhernan <60201738+fjhernan@users.noreply.github.com>
2020-10-14 16:02:46 -0700

    Added some more ui features
    Added a health bar and a money counter.

--- Virtual-Waste-Reclamation-Simulation/Assets/Scripts/Info.cs
+++ Virtual-Waste-Reclamation-Simulation/Assets/Scripts/Info.cs
@@ -3,24 +3,38 @@ using System.Collections.Generic;
 using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Info : MonoBehaviour
 {
     public TextMeshProUGUI info;
+    public Image image;
     // Start is called before the first frame update
     void Start()
     {
         info.text = "";
+        image = GetComponentInChildren<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     public void changeInfo(string info)
     {
         this.info.text = info;
+    }
+
+    public void changeInfo(string info, Color c)
+    {
+        this.info.faceColor = c;
+        this.info.text = info;
+    }
+
+    public void changeSprite(Sprite sprite)
+    {
+        image.sprite = sprite;
     }
 }
---
+++ Virtual-Waste-Reclamation-Simulation/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private string spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSpawn(string ots){
+        spawn = ots;
+        Debug.Log(spawn);
+    }
+
+    public string GetObject(){
+        string temp = spawn;
+        return temp;
+    }
+}
