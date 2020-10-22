ce7e7c77169814ed0db5030fef338c52718af9fc
lizardgai4 <70610229+lizardgai4@users.noreply.github.com>
2020-10-21 20:55:24 -0700

    Finished reimplementing the grid, and fixed the bizarre spawning bug

--- Riley's Project Space/Waste Project/Assets/Scripts/GridManagerCode.cs
+++ Riley's Project Space/Waste Project/Assets/Scripts/GridManagerCode.cs
@@ -12,8 +12,8 @@ public class GridManagerCode : MonoBehaviour
     public GameObject tileTexture;
     public GameObject blankSpace;
     public GameObject circle;
-    public List<List<Vector2>> paths; //this is a 2D array so as to support multiple paths
-    public Dictionary<Vector2, int> findPath; //use it to find out which path
+    public List<List<int[]>> paths; //this is a 2D array so as to support multiple paths
+    public List<List<int>> findPath; //use it to find out which path
     public Dictionary<float, int> findX;
     public Dictionary<float, int> findY;
     private int newCounter = 0;
@@ -24,20 +24,6 @@ public class GridManagerCode : MonoBehaviour
     public class Something {
         public int hi;
     }
-
-    /*public class GameTile
-    {
-        private int xPos { get; }
-        private int yPos { get; }
-        private GameObject tile;
-
-        public GameTile(int x, int y, GameObject newTile)
-        {
-            xPos = x;
-            yPos = y;
-            tile = newTile;
-        }
-    }*/
 
     // Start is called before the first frame update
     void Start()
@@ -62,58 +48,62 @@ public class GridManagerCode : MonoBehaviour
 
         float xPos, yPos;
 
-        for (int runnerX = 0; runnerX < rows * 2; runnerX++)
+        findPath = new List<List<int>>();
+
+        for (int runnerX = 0; runnerX < rows; runnerX++)
         {
             //List<GameTile> temp = new List<GameTile>();
             grid.Add(new List<GameObject>());
 
             GameObject temp = new GameObject();
 
-            xPos = (runnerX * -tileSize + offset - 1 + rows) * -1;
+            xPos = (runnerX * -tileSize + offset - 1 + rows/2) * -1;
+
+            findPath.Add(new List<int>());
 
             findX.Add(xPos,runnerX);
 
-            for (int runnerY = 0; runnerY < columns * 2; runnerY++)
+            //findPath = new List<List<int>>();
+
+            for (int runnerY = 0; runnerY < columns; runnerY++)
             {
-                //GameObject tile;
+                yPos = (runnerY * tileSize + offset - columns/2) * -1;
 
-                yPos = (runnerY * tileSize + offset - columns) * -1;
-
-                grid.Add(new List<GameObject>());
+                //grid.Add(new List<GameObject>());
                 if (firstTile) {
                     grid[0].Add((GameObject)Instantiate(circle, transform));
                     grid[0][0].name = "OpenTile " + "(" + counter + ")";
                     //fancy instantiation of paths
-                    paths = new List<List<Vector2>>();
-                    paths.Add(new List<Vector2>());
-                    paths[0].Add(new Vector2 (xPos, yPos));
+                    paths = new List<List<int[]>>();
+                    paths.Add(new List<int[]>());
+                    paths[0].Add(new int[] { 0, 0 });
                     //fancy instantiation of paths
-                    findPath = new Dictionary<Vector2, int>();
-                    findPath.Add(paths[0][0], 0);
-                    grid[0][0].transform.position = paths[0][0];
+                    
+                    
+                    findPath[0].Add(0);
+                    grid[0][0].transform.position = new Vector2(xPos, yPos);
                     //grid[0][0] = tile;
                     firstTile = false;
+                    if (runnerX == 0) {
+                        findY.Add(yPos, runnerY);
+                    }
                 }
                 else {
+                    findPath[runnerX].Add(0);
+
                     grid[runnerX].Add((GameObject)Instantiate(blankSpace, transform));
                     grid[runnerX][runnerY].name = "BlockedTile " + "(" + counter + ")";
                     grid[runnerX][runnerY].transform.position = new Vector2(xPos, yPos);
+
+                    if (runnerX == 0)
+                    {
+                        findY.Add(yPos, runnerY);
+                    }
                 }
                 
                 counter++;
-                
-                //Instantiate(tile);
-                //string outing = xPos.ToString() + "-" + yPos.ToString();
-                //Debug.Log(outing);
-
-                //grid.Add(temp);
-                //temp.Add(new GameTile(runnerX,runnerY,tile));
             }
-
-            //grid.Add(temp);
         }
-
-
     }
     // Update is called once per frame
     void Update()
@@ -127,120 +117,102 @@ public class GridManagerCode : MonoBehaviour
 
             if (hit && hit.collider.gameObject.name.Contains("OpenTile"))
             {
-                    
                 Vector2 center = GameObject.Find(hit.collider.gameObject.name).transform.position;
-                Ray[] UpDownLeftRight = {
-                    new Ray(new Vector3(ray.origin.x, ray.origin.y + tileSize, ray.origin.z), ray.direction),
-                    new Ray(new Vector3(ray.origin.x, ray.origin.y - tileSize, ray.origin.z), ray.direction),
-                    new Ray(new Vector3(ray.origin.x - tileSize, ray.origin.y, ray.origin.z), ray.direction),
-                    new Ray(new Vector3(ray.origin.x + tileSize, ray.origin.y, ray.origin.z), ray.direction),
-                };
-
-                //If there is no filledTileEnd around it, turn into a blocked tile.
-                /* (Ray i in UpDownLeftRight) {
-                    RaycastHit2D hit2 = Physics2D.Raycast(i.origin, i.direction);
-                    if (!hit2) {
-                        continue;
-                    }
-
-                }*/
 
                 print(hit.collider.name);
 
-                GameObject currentTile = GameObject.Find(hit.collider.gameObject.name);
+                int[] clickPlace = { findX[center.x], findY[center.y] };
 
-                foreach (Ray i in UpDownLeftRight) {
-                    /*if (i.x > columns || i.x < -columns || i.y > rows || i.y < -rows) {
-                        continue;
-                    }*/
-                    RaycastHit2D hit2 = Physics2D.Raycast(i.origin, i.direction);
+                int[][] upDownLeftRight = {
+                    new int[] {clickPlace[0], clickPlace[1] - 1}, //up
+                    new int[] {clickPlace[0], clickPlace[1] + 1}, //down
+                    new int[] {clickPlace[0] - 1, clickPlace[1]}, //left
+                    new int[] {clickPlace[0] + 1, clickPlace[1]}  //right
+                };
 
-                    if (!hit2)
-                        continue;
+                foreach (int[] i in upDownLeftRight)
+                {
+                    if (i[0] < 0 ||
+                        i[0] >= rows ||
+                        i[1] < 0 ||
+                        i[1] >= columns
+                    ) { continue; } 
 
-                    GameObject currentTile2 = GameObject.Find(hit2.collider.gameObject.name);
-
-                    if (hit2.collider.gameObject.name.Contains("BlockedTile")) {
-                        Destroy(GameObject.Find(hit2.collider.gameObject.name));
-                        GameObject newTile2 = (GameObject)Instantiate(circle, transform);
-                        newTile2.name = "OpenTile " + "(" + newCounter + ")";
+                    if (grid[i[0]][i[1]].name.Contains("BlockedTile"))
+                    {
+                        Vector2 temp = grid[i[0]][i[1]].transform.position;
+                        Destroy(grid[i[0]][i[1]]);
+                        
+                        GameObject tempTile = (GameObject)Instantiate(circle, transform);
+                        grid[i[0]][i[1]] = tempTile;
+                        grid[i[0]][i[1]].name = "OpenTile " + "(" + newCounter + ")";
 
                         newCounter++;
-                        newTile2.transform.position = new Vector2(currentTile2.transform.position.x, currentTile2.transform.position.y);
+                        grid[i[0]][i[1]].transform.position = temp;
                     }
-                    else if (hit2.collider.gameObject.name.Contains("FilledTileEnd"))
+                    else if (grid[i[0]][i[1]].name.Contains("FilledTileEnd"))
                     {
-                        Vector3 newPos = currentTile2.transform.position;
-
-                        Destroy(GameObject.Find(hit2.collider.gameObject.name));
-                        GameObject newTile2 = (GameObject)Instantiate(tileTexture, transform);
-                        newTile2.name = "FilledTileConduit " + "(" + endCounter + ")";
+                        Vector2 temp = grid[i[0]][i[1]].transform.position;
+                        //Vector3 newPos = currentTile2.transform.position;
+                        Destroy(grid[i[0]][i[1]]);
+                        GameObject tempTile = (GameObject)Instantiate(tileTexture, transform);
+                        grid[i[0]][i[1]] = tempTile;
+                        grid[i[0]][i[1]].name = "FilledTileConduit " + "(" + endCounter + ")";
+                        grid[i[0]][i[1]].transform.position = temp;
                         //findPath.Add();
-                        paths[findPath[newPos]].Add(center);
-                        findPath.Add(center,findPath[newPos]);
+                        findPath[clickPlace[0]][clickPlace[1]] = findPath[i[0]][i[1]];
+                        //findPath.Add(clickPlace, findPath[i]);
 
                         endCounter++;
-                        newTile2.transform.position = currentTile2.transform.position;
+                        //newTile2.transform.position = currentTile2.transform.position;
 
-                        Ray[] UpDownLeftRight2 = {
-                            new Ray(new Vector3(newPos.x, newPos.y + 1, newPos.z), ray.direction),
-                            new Ray(new Vector3(newPos.x, newPos.y - 1, newPos.z), ray.direction),
-                            new Ray(new Vector3(newPos.x - 1, newPos.y, newPos.z), ray.direction),
-                            new Ray(new Vector3(newPos.x + 1, newPos.y, newPos.z), ray.direction),
+                        int[][] upDownLeftRight2 = {
+                            new int[] {i[0], i[1] - 1}, //up
+                            new int[] {i[0], i[1] + 1}, //down
+                            new int[] {i[0] - 1, i[1]}, //left
+                            new int[] {i[0] + 1, i[1]}  //right
                         };
 
-                        foreach (Ray j in UpDownLeftRight2)
+                        foreach (int[] j in upDownLeftRight2)
                         {
-                            RaycastHit2D hit3 = Physics2D.Raycast(j.origin, i.direction);
+                            //RaycastHit2D hit3 = Physics2D.Raycast(j.origin, i.direction);
 
-                            if (!hit3 || hit3.transform.position.Equals(hit.collider.gameObject.transform.position) || hit3.collider.gameObject.name.Contains("FilledTileConduit"))
-                                continue;
+                            //if (!hit3 || hit3.transform.position.Equals(hit.collider.gameObject.transform.position) || hit3.collider.gameObject.name.Contains("FilledTileConduit"))
+                            //continue;
 
-                            GameObject currentTile3 = GameObject.Find(hit3.collider.gameObject.name);
+                            if (j[0] < 0 ||
+                                j[0] >= rows ||
+                                j[1] < 0 ||
+                                j[1] >= columns ||
+                                (j[0] == clickPlace[0] && j[1] == clickPlace[1])
+                            ) { continue; }
 
-                            if (hit3.collider.gameObject.name.Contains("OpenTile"))
+                            //GameObject currentTile3 = GameObject.Find(hit3.collider.gameObject.name);
+
+                            if (grid[j[0]][j[1]].name.Contains("OpenTile"))
                             {
-                                Destroy(GameObject.Find(hit3.collider.gameObject.name));
-                                GameObject newTile3 = (GameObject)Instantiate(blankSpace, transform);
-                                newTile3.name = "BlockedTile " + "(" + newCounter + ")";
+                                Vector2 temp2 = grid[j[0]][j[1]].transform.position;
+                                Destroy(grid[j[0]][j[1]]);
+                                GameObject tempTile2 = (GameObject)Instantiate(blankSpace, transform);
+                                grid[j[0]][j[1]] = tempTile2;
+                                grid[j[0]][j[1]].name = "BlockedTile " + "(" + newCounter + ")";
 
                                 newCounter++;
-                                newTile3.transform.position = new Vector2(currentTile3.transform.position.x, currentTile3.transform.position.y);
+                                grid[j[0]][j[1]].transform.position = temp2;
                             }
                         }
                     }
                 }
 
-                Vector2 newPosition = GameObject.Find(hit.collider.gameObject.name).transform.position;
-                Destroy(GameObject.Find(hit.collider.gameObject.name));
+                Vector2 newPosition = grid[clickPlace[0]][clickPlace[1]].transform.position;
+                Destroy(grid[clickPlace[0]][clickPlace[1]]);
                 GameObject newTile = (GameObject)Instantiate(tileTexture, transform);
+                grid[clickPlace[0]][clickPlace[1]] = newTile;
                 newTile.name = "FilledTileEnd " + "(" + newCounter + ")";
 
                 newCounter++;
                 newTile.transform.position = newPosition;
-
-                //GameObject newTile = (GameObject)Instantiate(blankSpace, GameObject.Find(hit.collider.gameObject.name.position));
-                //Destroy(GameObject.Find(hit.collider.gameObject.name));
-
-                //Destroy(GameObject.Find(hit.collider.gameObject.name));
-                //GameObject.Find(hit.collider.gameObject.name) = (GameObject)Instantiate(tileTexture, transform);
-                //GameObject tempHolder = GameObject.Find(hit.collider.gameObject.name);
-                //tempHolder = (GameObject)Instantiate(tileTexture, transform);
-                //transform.position = new Vector2();
-                //Vector3 tp = GameObject.Find(hit.collider.gameObject.name).positon;
-                //Destroy(GameObject.Find(hit.collider.gameObject.name));
-                //GameObject replacing = (GameObject)Instantiate(tileTexture, tp);
             }
-
-            /*
-            if (Physics.Raycast(ray, out hit))
-            {
-                Collider2D bc = hit.collider2D as Collider2D;
-                if (bc != null)
-                {
-                    Destroy(bc.gameObject);
-                }
-            }*/
         }
     }
 
